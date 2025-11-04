955f44b7235d835e1118ad6e504b944401b4b214
Chiara Dent <chiara.dent@t-online.de>
2025-11-04 17:37:31 +0100

    Mobile Navigation - Grundgerüst des Hamburgermenüs

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,9 @@
-$("header").load("components/header.html", );
+$("header").load("components/header.html", function() {
+
+  const hamburger = document.querySelector('.hamburger');
+  const navList = document.querySelector('.navigation-list-header');
+
+  hamburger.addEventListener('click', () => {
+    navList.classList.toggle('active'); 
+  });
+});
