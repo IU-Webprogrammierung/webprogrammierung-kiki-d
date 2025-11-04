33c3239d4552f44ef689dde9fc171aac1ff7cb95
Chiara Dent <chiara.dent@t-online.de>
2025-11-04 19:32:09 +0100

    Strukturanpassung - Änderung auf Onepager - Löschung der einzelnen Unterseiten

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,17 @@
 $("header").load("components/header.html", function() {
 
-  const hamburger = document.querySelector('.hamburger');
+ const hamburger = document.querySelector('.hamburger');
   const navList = document.querySelector('.navigation-list-header');
 
   hamburger.addEventListener('click', () => {
-    navList.classList.toggle('active'); 
-    hamburger.classList.toggle('active');
-    document.body.classList.toggle('menu-open');
+    navList.classList.toggle('active'); // Menü auf-/zuklappen
+  });
+
+  // Optional: Menü nach Klick auf Link schließen
+  document.querySelectorAll('.navigation-list-header a').forEach(link => {
+    link.addEventListener('click', () => {
+      navList.classList.remove('active');
+    });
   });
 });
 
